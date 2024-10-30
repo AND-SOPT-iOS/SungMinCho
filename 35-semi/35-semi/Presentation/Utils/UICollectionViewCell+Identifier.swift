092ffd9291245f63ab42104cab57c119f4ef989b
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-10-30 15:55:50 +0900

    feat : CollectionViewIdentifier extension

---
+++ 35-semi/35-semi/Presentation/Utils/UICollectionViewCell+Identifier.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+Identifier.swift
+//  35-semi
+//
+//  Created by 조성민 on 10/29/24.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        String(describing: self)
+    }
+}
