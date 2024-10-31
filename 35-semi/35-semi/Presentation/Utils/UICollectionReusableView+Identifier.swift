50d11d64d192561bf19937d9c44a16e580d3077f
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-11-01 04:31:11 +0900

    feat : 재사용 Identifier

---
+++ 35-semi/35-semi/Presentation/Utils/UICollectionReusableView+Identifier.swift
@@ -0,0 +1,16 @@
+//
+//  UICollectionReusableView+Identifier.swift
+//  35-semi
+//
+//  Created by 조성민 on 11/1/24.
+//
+
+import UIKit
+
+extension UICollectionReusableView {
+    
+    static var headerIdentifier: String {
+        String(describing: self)
+    }
+    
+}
--- 35-semi/35-semi/Presentation/Utils/UICollectionViewCell+Identifier.swift
+++ 35-semi/35-semi/Presentation/Utils/UICollectionViewCell+Identifier.swift
@@ -8,7 +8,9 @@
 import UIKit
 
 extension UICollectionViewCell {
-    static var identifier: String {
+    
+    static var cellIdentifier: String {
         String(describing: self)
     }
+    
 }
