a1663a61bb8bbe32030db554f7c62dd57bf43635
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-11-02 17:19:48 +0900

    feat : DTO 작성

---
+++ 35-semi/35-semi/Network/DTO/Request/LoginDTO.swift
@@ -0,0 +1,13 @@
+//
+//  LoginDTO.swift
+//  35-semi
+//
+//  Created by 조성민 on 11/2/24.
+//
+
+struct LoginDTO: Encodable {
+    
+    let username: String
+    let password: String
+    
+}
---
+++ 35-semi/35-semi/Network/DTO/Request/RegisterDTO.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterDTO.swift
+//  35-semi
+//
+//  Created by 조성민 on 11/2/24.
+//
+
+struct RegisterDTO: Encodable {
+    
+    let username: String
+    let password: String
+    let hobby: String
+    
+}
