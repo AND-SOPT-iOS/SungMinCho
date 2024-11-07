38d315f46e20d12beb6b37a30c83bff58d578f08
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-11-07 10:44:36 +0900

    feat : UITextField 스타일 extension

---
+++ 35-semi/35-semi/Presentation/Utils/UITextField+SetStyle.swift
@@ -0,0 +1,27 @@
+//
+//  UITextField+SetStyle.swift
+//  35-semi
+//
+//  Created by 조성민 on 11/4/24.
+//
+
+import UIKit
+
+extension UITextField {
+    
+    func setStyle() {
+        autocapitalizationType = .none
+        autocorrectionType = .no
+        layer.borderWidth = 0.5
+        layer.cornerRadius = 10
+        clearButtonMode = .whileEditing
+        returnKeyType = .done
+        
+        let spacingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        leftView = spacingView
+        rightView = spacingView
+        rightViewMode = .always
+        leftViewMode = .always
+    }
+    
+}
