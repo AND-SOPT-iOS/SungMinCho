0f4066920c639cff9cedb99b29f5e97f2e2c74f5
SungMinCho-Kor <rhxoeh0011@gmail.com>
2024-12-10 01:42:06 +0900

    feat: SwiftUI 프로젝트 생성

---
+++ SeminarSwiftUI/SeminarSwiftUI/Rank/RankView.swift
@@ -0,0 +1,18 @@
+//
+//  RankView.swift
+//  SeminarSwiftUI
+//
+//  Created by 조성민 on 12/10/24.
+//
+
+import SwiftUI
+
+struct RankView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RankView()
+}
